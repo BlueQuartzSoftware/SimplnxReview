296afc26bb168718ef6d33c36c4d041300c133ed
Michael Jackson <mike.jackson@bluequartz.net>
2026-02-25 23:45:00 -0500

    FILT: Promote Compute Grouping Density to SimplnxCore

--- src/SimplnxReview/SimplnxReviewLegacyUUIDMapping.hpp
+++ src/SimplnxReview/SimplnxReviewLegacyUUIDMapping.hpp
@@ -10,7 +10,6 @@
 #include "SimplnxReview/Filters/ComputeSaltykovSizesFilter.hpp"
 #include "SimplnxReview/Filters/ComputeMicroTextureRegionsFilter.hpp"
 #include "SimplnxReview/Filters/ComputeLocalAverageCAxisMisalignmentsFilter.hpp"
-#include "SimplnxReview/Filters/ComputeGroupingDensityFilter.hpp"
 
 // @@__HEADER__TOKEN__DO__NOT__DELETE__@@
 
@@ -27,7 +26,6 @@ namespace nx::core
     {nx::core::Uuid::FromString("cc76cffe-81ad-5ece-be2a-ce127c5fa6d7").value(), {nx::core::FilterTraits<ComputeSaltykovSizesFilter>::uuid, &ComputeSaltykovSizesFilter::FromSIMPLJson}}, // FindSaltykovSizes
     {nx::core::Uuid::FromString("90f8e3b1-2460-5862-95a1-a9e06f5ee75e").value(), {nx::core::FilterTraits<ComputeMicroTextureRegionsFilter>::uuid, &ComputeMicroTextureRegionsFilter::FromSIMPLJson}}, // FindMicroTextureRegions
     {nx::core::Uuid::FromString("49b2dd47-bb29-50d4-a051-5bad9b6b9f80").value(), {nx::core::FilterTraits<ComputeLocalAverageCAxisMisalignmentsFilter>::uuid, &ComputeLocalAverageCAxisMisalignmentsFilter::FromSIMPLJson}}, // FindLocalAverageCAxisMisalignments
-    {nx::core::Uuid::FromString("708be082-8b08-4db2-94be-52781ed4d53d").value(), {nx::core::FilterTraits<ComputeGroupingDensityFilter>::uuid, &ComputeGroupingDensityFilter::FromSIMPLJson}}, // FindGroupingDensity
     // @@__MAP__UPDATE__TOKEN__DO__NOT__DELETE__@@
   };
 
