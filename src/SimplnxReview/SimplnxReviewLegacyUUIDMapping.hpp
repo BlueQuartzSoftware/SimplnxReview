ee860cbd4b66ffcd2188e2d1d122adfc69edc749
Michael Jackson <mike.jackson@bluequartz.net>
2026-06-24 16:23:48 -0400

    FILT: Move GroupMicrotextureRegionsFilter to SIMPLNX/OrientationAnalysis (#21)

--- src/SimplnxReview/SimplnxReviewLegacyUUIDMapping.hpp
+++ src/SimplnxReview/SimplnxReviewLegacyUUIDMapping.hpp
@@ -5,7 +5,6 @@
 #include <nlohmann/json.hpp>
 
 /* clang-format off */
-#include "SimplnxReview/Filters/GroupMicroTextureRegionsFilter.hpp"
 #include "SimplnxReview/Filters/MergeColoniesFilter.hpp"
 #include "SimplnxReview/Filters/ComputeSaltykovSizesFilter.hpp"
 #include "SimplnxReview/Filters/ComputeMicroTextureRegionsFilter.hpp"
@@ -21,7 +20,6 @@ namespace nx::core
   static const AbstractPlugin::SIMPLMapType k_SIMPL_to_SimplnxReview
   {
     // syntax std::make_pair {Dream3d UUID , Dream3dnx UUID, {}}}, // dream3d-class-name
-    {nx::core::Uuid::FromString("5e18a9e2-e342-56ac-a54e-3bd0ca8b9c53").value(), {nx::core::FilterTraits<GroupMicroTextureRegionsFilter>::uuid, &GroupMicroTextureRegionsFilter::FromSIMPLJson}}, // GroupMicroTextureRegions
     {nx::core::Uuid::FromString("2c4a6d83-6a1b-56d8-9f65-9453b28845b9").value(), {nx::core::FilterTraits<MergeColoniesFilter>::uuid, &MergeColoniesFilter::FromSIMPLJson}}, // MergeColonies
     {nx::core::Uuid::FromString("cc76cffe-81ad-5ece-be2a-ce127c5fa6d7").value(), {nx::core::FilterTraits<ComputeSaltykovSizesFilter>::uuid, &ComputeSaltykovSizesFilter::FromSIMPLJson}}, // FindSaltykovSizes
     {nx::core::Uuid::FromString("90f8e3b1-2460-5862-95a1-a9e06f5ee75e").value(), {nx::core::FilterTraits<ComputeMicroTextureRegionsFilter>::uuid, &ComputeMicroTextureRegionsFilter::FromSIMPLJson}}, // FindMicroTextureRegions
